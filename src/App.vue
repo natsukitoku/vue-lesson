7298d29e6499b59d090ed2ec52adc712bc3a5097
Natsuki Tokunaga <marron0831@outlook.jp>
2025-02-17 08:21:52 +0900

    ref,reactive使用例

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,45 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref, reactive } from 'vue'
+const title = ref('Vue.js Course')
+let price = ref(9.99)
+function increment() {
+  price.value += 1
+  instructor.age += 1
+  instructor.bio = 'hi'
+  instructor.sns.twitter = 'hello'
+  instructor.email = ref('hellooo')
+}
+const info = ref({
+  students: 1000,
+  rating: 4
+})
+const instructor = reactive({
+  name: 'Natsuki',
+  age: 25,
+  sns: {
+    twitter: '@abc',
+    youtube: '@abc',
+  },
+  email: ref('abc@abc.com')
+})
+instructor.bio = 'hello'
+const items = reactive([ref(1), ref(2), ref(3)])
+items.reverse()
+const courseInfo = {
+  sections: ref(10),
+  language: ref('Japanese')
+}
+const sections = courseInfo.sections
+console.log(courseInfo.sections.value)
 </script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <h1>Title: {{ title }}</h1>
+  <h2>Price: ${{ price - 1 }}</h2>
+  <button @click="increment">button</button>
+  <h2>Students: {{ info.students }}</h2>
+  <h2>Instructor age: {{ instructor.age }}</h2>
+  <h2>Instructor bio: {{ instructor.bio }}</h2>
+  <h2>Instructor Twitter: {{ instructor.sns.twitter }}</h2>
+  <h2>Instructor email: {{ instructor.email }}</h2>
+  <h2>Course Info Sections: {{ sections + 3 }}</h2>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
