9f509f0b195bd0de68df5b2fbd6386d5f5330da9
Natsuki Tokunaga <marron0831@outlook.jp>
2025-02-17 08:51:04 +0900

    v-bind使用例

---
+++ src/Example.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(2)
+const count2 = ref(4)
+const message = ref('<h1>Hello</h1>')
+const vueURL = ref('https://vuejs.org')
+const vueId = ref('vue-link')
+</script>
+<template>
+  <div>{{ count + count2 }}</div>
+  <div>{{ count > 3 ? 'Yes' : 'No' }}</div>
+  <div v-text="count"></div>
+  <!-- v-htmlは悪意のあるユーザーにhtmlを書き換えられてしまう可能性があるのでユーザーから送られてきたデータには使用しない(XSS攻撃対策) -->
+  <div v-html="message"></div>
+  <a :id="vueId" :href="vueURL">Vue.js</a>
+  <a v-bind="{id: vueId, href: vueURL }">Vue.js</a>
+  <button :disabled="true">button</button>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import Example from "./Example.vue";
 const app = createApp(App);
+const exampleApp = createApp(Example);
 app.mount('#app');
+exampleApp.mount('#example');
