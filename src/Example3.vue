1b7c865f9bf58f59d3d2c4eda4ec7aab5088e283
Natsuki Tokunaga <marron0831@outlook.jp>
2025-02-17 22:55:27 +0900

    イベント修飾子

---
+++ src/Example3.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+const count = ref(0)
+</script>
+<template>
+  <p>{{ count }}</p>
+  <div @click="count++">
+    <button @click.stop>button</button>
+    <a @click.prevent.stop href="https://vuejs.org">Vue.js</a>
+  </div>
+</template>
