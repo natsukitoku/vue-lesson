bb52b0c16022555d9f3a81532c657f37063b159e
Natsuki Tokunaga <marron0831@outlook.jp>
2025-02-17 23:02:40 +0900

    キー修飾子

---
+++ src/Example4.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from 'vue';
+const count =ref(0)
+const eventName = 'keyup'
+</script>
+<template>
+  <p>{{ count }}</p>
+  <!-- <input type="text" @keyup.space.delete="count++"> -->
+  <input type="text" @[eventName].space.delete="count++">
+</template>
